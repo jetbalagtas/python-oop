73bffd337a8df4f1738a6e512670ac1f8a8c5a3a
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-14 20:22:44 -0400

    contructor and instance attributes

--- car.py
+++ car.py
@@ -1,6 +1,9 @@
 class Car:
-    top_speed = 150
-    warnings = []
+    # top_speed = 150
+    # warnings = []
+    def __init__(self, starting_top_speed=100):
+        self.top_speed = starting_top_speed
+        self.warnings = []
 
     def drive(self):
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
@@ -11,8 +14,10 @@ car1.drive()
 # Car.top_speed = 200
 car1.warnings.append('New warning')
 
-car2 = Car()
+car2 = Car(200)
 car2.drive()
+print(car2.warnings)
 
-car3 = Car()
+car3 = Car(250)
 car3.drive()
+print(car3.warnings)

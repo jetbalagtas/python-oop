3db92c8074b4da57671b6c5e9900e68e49167859
Jet Balagtas <jetbalagtas@gmail.com>
2018-10-14 20:20:23 -0400

    understanding class attributes

--- car.py
+++ car.py
@@ -1,8 +1,18 @@
 class Car:
     top_speed = 150
+    warnings = []
 
     def drive(self):
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
 
 car1 = Car()
 car1.drive()
+
+# Car.top_speed = 200
+car1.warnings.append('New warning')
+
+car2 = Car()
+car2.drive()
+
+car3 = Car()
+car3.drive()
